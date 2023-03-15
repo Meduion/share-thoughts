da501abed3bd0971a90f347785b960846dfa6b25
Brian Wyatt <brianlwyatt@gmail.com>
2023-03-14 22:19:01 -0300

    minor change

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const cwd = process.cwd();
-
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const activity = cwd.includes
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,6 +11,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server now running on ${PORT}!`);
   });
 });
